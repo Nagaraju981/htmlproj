8fadae28a6daafbeb545099bf30f4edd347bca2c
Nagaraju981 <mahindra150981@gmail.com>
2024-12-30 21:37:11 +0530

    Update settings.py

--- htmlformproj/settings.py
+++ htmlformproj/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-aanb)8ky((z1@c$zylwzm&34n3g$(r9thm8l$(ki@mqehoa*b1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
